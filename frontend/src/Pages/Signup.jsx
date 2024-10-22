6c70ea048ece705eaff03df9ee795ec41241d377
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-10-22 20:28:26 +0530

    fixed subheading

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -3,7 +3,7 @@ import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
-import { SubHeading } from "../components/Subheading";
+import { Subheading } from "../components/Subheading";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -20,7 +20,7 @@ export const Signup = () => {
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
-          <SubHeading label={"Enter your infromation to create an account"} />
+          <Subheading label={"Enter your infromation to create an account"} />
           <InputBox
             onChange={(e) => {
               setFirstName(e.target.value);
